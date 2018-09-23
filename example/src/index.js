16cb3bd335c414a50a8aeb56a437cbbe1d93fdee
Matt Garnett <matt@mattgarnett.com>
2018-09-23 18:23:48 -0500

    working on example project

--- example/src/index.js
+++ example/src/index.js
@@ -1,28 +1,88 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import Counter from './components/Counter'
-import counter from './reducers'
-import { reducers as smiteReducers } from 'smite'
+import counter from './reducer'
+// import { reducers as smiteReducers } from 'smite'
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.incrementAsync = this.incrementAsync.bind(this);
+    this.incrementIfOdd = this.incrementIfOdd.bind(this);
+  }
+
+  incrementIfOdd() {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement()
+    }
+  }
+
+  incrementAsync() {
+    setTimeout(this.props.onIncrement, 1000)
+  }
+
+  render() {
+    const { store, onIncrement, onDecrement } = this.props
+
+    let value = 0
+    if (store) {
+      value = store.counter.value
+    }
+
+    return (
+      <p>
+        Clicked: {value} times
+        {' '}
+        <button onClick={onIncrement}>
+          +
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          -
+        </button>
+        {' '}
+        <button onClick={this.incrementIfOdd}>
+          Increment if odd
+        </button>
+        {' '}
+        <button onClick={this.incrementAsync}>
+          Increment async
+        </button>
+      </p>
+    )
+  }
+}
 
 const store = createStore(
-	  combineReducers({
-    	counter: counter,
-    	...smiteReducers,
-  	}),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    combineReducers({
+      counter: counter,
+      // ...smiteReducers,
+    }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-
-const rootEl = document.getElementById('root')
 
 const render = () => ReactDOM.render(
   <Counter
-    value={store.getState()}
+    store={store.getState()}
     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
   />,
-  rootEl
+  document.getElementById('app')
 )
 
-render()
+ReactDOM.render(
+  <div>
+    <Counter
+      // value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />
+  </div>
+  ,
+  document.getElementById('app')
+);
+
+
 store.subscribe(render)
+
+module.hot.accept();
---
+++ example/src/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+	value: 0
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, value: state.value + 1 }
+    case 'DECREMENT':
+      return { ...state, value: state.value - 1 }
+    default:
+      return state
+  }
+}
